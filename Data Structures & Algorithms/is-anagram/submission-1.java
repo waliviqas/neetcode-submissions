e4ec0d19ed58c6eba128d9fc1eae1f9c64557ffd
Wali Viqas <wali@viqas.com>
2026-07-10 19:36:04 -0400

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] frequency = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            frequency[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            frequency[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+
+            if (map.containsKey(difference)) {
+                return new int[]{map.get(difference), i};
+            }
+
+            map.put(nums[i], i);
+        
+        }
+
+        return null;
+        
+    }
+}
